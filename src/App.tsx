7e08e6583ecd85bcfed01cdb5baacad20a15ba7b
m-reinisch <michael.reinisch@email.de>
2026-06-18 11:54:53 +0200

    home and navigation implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
+import NavBar from "./NavBar.tsx";
+import Home from "./Home.tsx";
+import Canvas from "./Canvas.tsx";
+import {Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      {}
+        <header>
+            <h1>Todo-App</h1>
+            <NavBar />
+        </header>
+        <Routes>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/todos"} element={<Canvas />} />
+        </Routes>
     </>
   )
 }
---
+++ src/Canvas.tsx
@@ -0,0 +1,22 @@
+export default function Canvas() {
+    return(
+        <>
+            <table>
+                <thead>
+                <tr>
+                    <th>Open:</th>
+                    <th>In progress:</th>
+                    <th>Finished:</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </>
+    )
+}
