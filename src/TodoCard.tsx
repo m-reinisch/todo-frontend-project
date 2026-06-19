f3ccfd0dc98be567e51bf113966600da94b5203d
m-reinisch <michael.reinisch@email.de>
2026-06-19 09:35:45 +0200

    del() implemented

--- src/TodoCard.tsx
+++ src/TodoCard.tsx
@@ -6,33 +6,48 @@ type TodoProps= {
     change: () => void
 }
 
-export default function TodoCard(props: Readonly<TodoProps>) {
-    function levelUp(data: Todo){
-        const level: string= data.status
-        let stat: string= "OPEN"
+export function TodoCard(props: Readonly<TodoProps>) {
+    function levelUp(data: Todo) {
+        const level: string = data.status
+        let stat: string = "OPEN"
 
-        if (level === "OPEN"){
-            stat= "IN_PROGRESS"
-        } else if (level === "IN_PROGRESS"){
-            stat= "DONE"
+        if (level === "OPEN") {
+            stat = "IN_PROGRESS"
+        } else if (level === "IN_PROGRESS") {
+            stat = "DONE"
+        } else {
+            stat = "DONE"
         }
-        const upTodo: Todo= {
+        const upTodo: Todo = {
             id: data.id,
             description: data.description,
             status: stat
         }
         axios.put("/api/todo/" + data.id, upTodo)
-             .then( () => props.change() )
-             .catch( (errors) => console.log(errors) )
+             .then(() => props.change())
+             .catch((errors) => console.log(errors))
+    }
+    function del(id: string){
+        axios.delete("/api/todo/" + id)
+             .then(() => props.change())
+             .catch((errors) => console.log(errors))
     }
 
-    return(
+    return (
         <div id="card">
             <p id="desc">{props.todo.description}</p>
             <p id="stat">{props.todo.status}</p>
-            <button onClick={
+            <button type={"button"} onClick={
                 () => levelUp(props.todo)
-            }>Level up</button>
+            }>Level up
+            </button>
+            <button id="del" type={"button"}
+                    disabled={
+                        props.todo.status !== "DONE"
+                    }
+                    onClick={
+                        () => {del(props.todo.id)}
+            }>Löschen</button>
         </div>
     )
 }
